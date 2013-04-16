2612c4cbb4f18651beb446e0f3e5464bb9850b5a
CyberDream <vinniehe@VINNIE-NB.fareast.corp.microsoft.com>
2013-04-16 23:52:10 +0800

    update

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var http = require('http')
 var express = require('express');
+
+var path = require('path');
 var mongoose = require('mongoose');
 
 var app = express();
 mongoose.connect('mongodb://MongoLab-a4:KX_CVZRYhr_OPO__Uo2A4kkNUhjttC2lQT6bGNQ59kw-@ds041157.mongolab.com:41157/MongoLab-a4');
+
+var Application = require('./models/application.js');
 
 var LogList = require('./models/loglist');
 var logList = new LogList();
@@ -15,8 +19,13 @@ var port = process.env.PORT || 1337;
 
 app.configure(function(){
 	app.set('port', port);
+	app.set('views', __dirname + '/views');
+ 	app.set('view engine', 'jade');
+
 	app.use(express.bodyParser());
+	app.use(express.methodOverride());
 	app.use(app.router);
+	app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.get('/application', function(req, res){
@@ -43,6 +52,13 @@ app.get('/log/:appId', function(req, res){
 
 app.post('/log', function (req, res) {
 	logList.addLog(req, res);
+});
+
+app.get('/', function(req, res){
+	Application.find(function(err, items){
+		console.log('after find');
+		res.render('index', { title: 'Log Viewer', apps:items});
+	});
 });
 
 app.listen(port, function(){
